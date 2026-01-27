6b41eb3e88f96685492b93487a8ab8ad86b0d3f1
Roberto Perkovic <roberto.perkovic01@gmail.com>
2026-01-27 18:49:49 +0100

    Javascript Hello world

---
+++ 2_JS/vjezba01/skripta.js
@@ -0,0 +1,13 @@
+console.info('Hello world 5');
+console.table(JSON.parse('{"name"'));
+
+console.log('O\'Neal');//\altgr+q
+console.log("O'Neal");
+
+console.log(`Hello
+    world
+    6`); //altgr+7
+
+
+
+document.getElementById('ispis').innerHTML='<a href="#">Poveznica</a>';
