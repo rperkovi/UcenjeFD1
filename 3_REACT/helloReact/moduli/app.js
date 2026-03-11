a47c9e3385f4eea9ddcd403771b150b846667cb8
Roberto Perkovic <roberto.perkovic01@gmail.com>
2026-03-11 18:42:19 +0100

    predlozak i reject upoznavanje

---
+++ 3_REACT/helloReact/moduli/app.js
@@ -0,0 +1,11 @@
+import pozdrav, { PI,zbroji,prva } from './matematika';
+
+
+
+
+
+console.log(PI)
+console.log(zbroji(2,5))
+console.log(pozdrav('Edunova'))
+console.log(prva())
+//console.log(druga)
--- 3_REACT/helloReact/src/App.jsx
+++ 3_REACT/helloReact/src/App.jsx
@@ -1,20 +1,21 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
-import {Container} from 'react-bootstrap'
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+
 import react from './assets/react.svg'
 import './App.css'
-import Table from 'react-bootstrap/Table';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Container } from 'react-bootstrap'
+
+
+
 function App() {
   
 
   return (
     
     <Container>
+      <a href="moduli/index.html">Moduli</a>
+      <hr />
+      Accordion
+      
        <Card style={{ width: '18rem' }}>
       <Card.Img variant="top" src={react} />
       <Card.Body>
@@ -56,29 +57,7 @@ function App() {
     </Table>
     </Card>
     </Container>
-    <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+   
    
   )
 }
