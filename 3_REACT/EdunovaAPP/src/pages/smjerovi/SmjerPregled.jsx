faf05c27eedeb508bc105665ed1121d030f4f8c9
Roberto Perkovic <roberto.perkovic01@gmail.com>
2026-03-18 18:56:00 +0100

    instal zavisnosti, numericke vrijednosti i statusa aktivan

--- 3_REACT/EdunovaAPP/src/pages/smjerovi/SmjerPregled.jsx
+++ 3_REACT/EdunovaAPP/src/pages/smjerovi/SmjerPregled.jsx
@@ -1,7 +1,67 @@
+import { useEffect, useState } from "react"
+import SmjerService from "../../services/smjerovi/SmjerService"
+import { Table } from "react-bootstrap"
+import { NumericFormat } from "react-number-format"
+import { GrValidate } from "react-icons/gr"
+
 export default function SmjerPregled(){
+
+
+    const [smjerovi, setSmjerovi] = useState([])
+
+
+    useEffect(()=>{
+        ucitajSmjerove()
+    },[])
+
+    async function ucitajSmjerove() {
+        await SmjerService.get().then((odgovor)=>{
+            setSmjerovi(odgovor.data)
+        })
+    }
+
+
     return(
         <>
-        Ovdje će se vidjeti smjerovi
+            <Table>
+                <thead>
+                    <tr>
+                        <th>Naziv</th>
+                        <th>Trajanje</th>
+                        <th>Cijena</th>
+                        <th>Datum Pokretanja</th>
+                        <th>Aktivan</th>
+                        <th>Akcija</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {smjerovi && smjerovi.map((smjer)=>(
+                        <tr>
+                            <td>{smjer.naziv}</td>
+                            <td>{smjer.trajanje}</td>
+                            <td>
+                                <NumericFormat 
+                                value={smjer.cijena}
+                                displayType={'text'}
+                                thousandSeparator='.'
+                                decimalSeparator=','
+                                suffix={'€'}
+                                decimalScale={2}
+                                fixedDecimalScale
+                                />
+                            </td>
+                            <td>{smjer.datumPokretanja}</td>
+                            <td>
+                                <GrValidate
+                                size={25}
+                                color={smjer.aktivan ? 'green' : 'red'}
+                                />
+                                </td>
+                            <td></td>
+                        </tr>
+                    ))}
+                </tbody>
+            </Table>
         </>
     )
 }
